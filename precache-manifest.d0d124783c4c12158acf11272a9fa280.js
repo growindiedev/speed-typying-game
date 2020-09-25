254eca907178d64dca5634cbb6b294f19b1535af
Abhishek Ranjan <rabhishekranjan@gmail.com>
2020-09-26 00:17:47 +0530

    Updates

---
+++ precache-manifest.d0d124783c4c12158acf11272a9fa280.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d60d05767f50a0b2df8a1b5b13f6b2d3",
+    "url": "/speed-typying-game/index.html"
+  },
+  {
+    "revision": "2c319b4588476bf24601",
+    "url": "/speed-typying-game/static/css/main.98ee3570.chunk.css"
+  },
+  {
+    "revision": "3111a74774cc357e555e",
+    "url": "/speed-typying-game/static/js/2.a7da58ab.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/speed-typying-game/static/js/2.a7da58ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c319b4588476bf24601",
+    "url": "/speed-typying-game/static/js/main.d182ee0e.chunk.js"
+  },
+  {
+    "revision": "4f7be71eb29ae1ffdaa8",
+    "url": "/speed-typying-game/static/js/runtime-main.93813991.js"
+  }
+]);
